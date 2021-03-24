b54c18e93e94d4a1f6cde663ee8bd3c228b410d9
zengfanrong <zfr@sunwaycrm.com>
2021-03-24 18:06:43 +0800

    解决关联查询问题

--- src/main/java/fishman/fish/wxdemo/mapper/DeptMapper.java
+++ src/main/java/fishman/fish/wxdemo/mapper/DeptMapper.java
@@ -1,11 +1,13 @@
 package fishman.fish.wxdemo.mapper;
 
 
+import fishman.fish.wxdemo.bean.Dept;
 import fishman.fish.wxdemo.bean.Test;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface BeanMapper {
-  Test findBeanByID(@Param("id") String id);
+public interface DeptMapper {
+
+  List<Dept> findDeptsByID();
 }
---
+++ src/main/java/fishman/fish/wxdemo/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package fishman.fish.wxdemo.mapper;
+
+
+import fishman.fish.wxdemo.bean.Dept;
+import fishman.fish.wxdemo.bean.Test;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TestMapper {
+  Test findTestByID(@Param("id") String id);
+}
